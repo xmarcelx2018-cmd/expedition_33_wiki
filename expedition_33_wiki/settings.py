e4e242d4e3e98f078de14aa2b5688f3abf9a3610
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-09-16 14:10:05 +0100

    Set up cloudinary to project

--- expedition_33_wiki/settings.py
+++ expedition_33_wiki/settings.py
@@ -36,6 +36,14 @@ ALLOWED_HOSTS = [
     ".herokuapp.com",
 ]
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Application definition
 
@@ -46,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'wiki',
 ]
 
--- wiki/models.py
+++ wiki/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class WikiEntry(models.Model):
     title = models.CharField(max_length=60)
     category = models.CharField(
-    max_length=20,
-    choices=[
-        ('character', 'Character'),
-        ('weapon', 'Weapon'),
-        ('location', 'Location'),
-    ]
-)
+        max_length=20,
+        choices=[
+            ('character', 'Character'),
+            ('weapon', 'Weapon'),
+            ('location', 'Location'),
+        ]
+    )
     author = models.ForeignKey(
-    User,
-    on_delete=models.CASCADE,
-    related_name='wiki_entries',
-)
+        User,
+        on_delete=models.CASCADE,
+        related_name='wiki_entries',
+    )
     content = models.TextField(max_length=2000)
-    image = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(
+        upload_to='wiki_images/',
+        blank=True,
+        null=True
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
