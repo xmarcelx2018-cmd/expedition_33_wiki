bddf7aadcc1e508f2580b821bab9be295f3ecb49
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-09-22 00:15:08 +0100

    Testing fix for bad lighthouse score

--- expedition_33_wiki/settings.py
+++ expedition_33_wiki/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import cloudinary
 
 if os.path.isfile("env.py"):
     import env
@@ -42,6 +43,13 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
     'SECURE': True,
 }
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True,
+)
 
 # Application definition
 
