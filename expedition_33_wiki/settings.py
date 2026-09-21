0016a51454bf5e46cbe329795183509ff5b084f6
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-09-22 00:03:04 +0100

    Use secure Cloudinary image URLs for lighthouse test

--- expedition_33_wiki/settings.py
+++ expedition_33_wiki/settings.py
@@ -40,6 +40,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
 }
 
 # Application definition
