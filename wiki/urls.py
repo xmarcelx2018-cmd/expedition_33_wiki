b9137596480f471b4487da490181ce5ef3ec5168
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-09-22 02:26:42 +0100

    Adjust url's, and settings to match python validator and update README correspondingly

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,9 +3,21 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('characters/', views.WikiList.as_view(category='character'), name='characters'),
-    path('weapons/', views.WikiList.as_view(category='weapon'), name='weapons'),
-    path('locations/', views.WikiList.as_view(category='location'), name='locations'),
+    path(
+        'characters/',
+        views.WikiList.as_view(category='character'),
+        name='characters'
+    ),
+    path(
+        'weapons/',
+        views.WikiList.as_view(category='weapon'),
+        name='weapons'
+    ),
+    path(
+        'locations/',
+        views.WikiList.as_view(category='location'),
+        name='locations'
+    ),
     path('create/', views.WikiCreate.as_view(), name='wiki_create'),
     path('my-entries/', views.MyEntries.as_view(), name='my_entries'),
     path('<int:pk>/edit/', views.WikiUpdate.as_view(), name='wiki_edit'),
